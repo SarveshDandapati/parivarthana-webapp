2adf16282c625e6f00e3a261d49c891e1825e74c
Sarvesh Dandapati <dsarvesh911@gmail.com>
2023-02-15 01:01:07 +0530

    update the image in data file

--- src/data.js
+++ src/data.js
@@ -3,43 +3,62 @@ export const dataset = [
         id:1,
         title: 'Product Name',
         price: 100,
-        linkImg: 'https://repository-images.githubusercontent.com/519348881/d411fd39-0622-443e-8082-8783ccf386c8'
+        linkImg: 'https://static.wixstatic.com/media/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg/v1/fill/w_399,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg'
     },
     {
         id:2,
         title: 'Product Name',
         price: 100,
-        linkImg: 'https://repository-images.githubusercontent.com/519348881/d411fd39-0622-443e-8082-8783ccf386c8'
+        linkImg: 'https://static.wixstatic.com/media/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg/v1/fill/w_399,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg'
     },
     {
         id:3,
         title: 'Product Name',
         price: 100,
-        linkImg: 'https://repository-images.githubusercontent.com/519348881/d411fd39-0622-443e-8082-8783ccf386c8'
+        linkImg: 'https://static.wixstatic.com/media/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg/v1/fill/w_399,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg'
     },
     {
         id:4,
         title: 'Product Name',
         price: 100,
-        linkImg: 'https://repository-images.githubusercontent.com/519348881/d411fd39-0622-443e-8082-8783ccf386c8'
+        linkImg: 'https://static.wixstatic.com/media/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg/v1/fill/w_399,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg'
     },
     {
         id:5,
         title: 'Product Name',
         price: 100,
-        linkImg: 'https://repository-images.githubusercontent.com/519348881/d411fd39-0622-443e-8082-8783ccf386c8'
+        linkImg: 'https://static.wixstatic.com/media/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg/v1/fill/w_399,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg'
     },
     {
         id:6,
         title: 'Product Name',
         price: 100,
-        linkImg: 'https://repository-images.githubusercontent.com/519348881/d411fd39-0622-443e-8082-8783ccf386c8'
+        linkImg: 'https://static.wixstatic.com/media/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg/v1/fill/w_399,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg'
     },
     {
         id:7,
         title: 'Product Name',
         price: 100,
-        linkImg: 'https://repository-images.githubusercontent.com/519348881/d411fd39-0622-443e-8082-8783ccf386c8'
+        linkImg: 'https://static.wixstatic.com/media/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg/v1/fill/w_399,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg'
+    },
+    {
+        id:8,
+        title: 'Product Name',
+        price: 100,
+        linkImg: 'https://static.wixstatic.com/media/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg/v1/fill/w_399,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg'
+    },
+    {
+        id:9,
+        title: 'Product Name',
+        price: 100,
+        linkImg: 'https://static.wixstatic.com/media/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg/v1/fill/w_399,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg'
+    },
+    {
+        id:10,
+        title: 'Product Name',
+        price: 100,
+        linkImg: 'https://static.wixstatic.com/media/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg/v1/fill/w_399,h_399,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_e3d737aca64f4010bda5abd4e75a37cf~mv2.jpeg'
     }
+
 
 ];
