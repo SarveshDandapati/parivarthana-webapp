92fdf8768c546daa5617664281eb62656f215c47
Sarvesh Dandapati <dsarvesh911@gmail.com>
2023-02-16 19:35:27 +0530

    fix typo from class to className

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,31 @@
 import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div class="container">
-      <footer class="py-3 my-4">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-          <li class="nav-item">
+    <div className="container">
+      <footer className="py-3 my-4">
+        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+          <li className="nav-item">
             <Link to="/" className="link-style nav-link px-2 text-muted">
               Home
             </Link>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <Link to="/about" className="link-style nav-link px-2 text-muted">
               About
             </Link>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <Link to="/contact" className="link-style nav-link px-2 text-muted">
               Contacts
             </Link>
           </li>
-          <li class="nav-item">
+          <li className="nav-item">
             <Link to="/product" className="link-style nav-link px-2 text-muted">
               Products
             </Link>
           </li>
         </ul>
-        <p class="text-center text-muted">Parivarthana, A CSA Venture</p>
+        <p className="text-center text-muted">Parivarthana, A CSA Venture</p>
       </footer>
     </div>
   );
