06454dc4fd54090e9bf4daa99ab1ab3a987a5438
Sarvesh Dandapati <dsarvesh911@gmail.com>
2023-02-22 23:56:12 +0530

    Revert "signup Form , added axios for future use."
    This reverts commit f757d858577e27a838c747145636e59cadc1b049.

--- src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-axios.defaults.headers.post["Content-Type"]="application/json";
-var mainAxios = axios.create({
-    baseUrl: "http://localhost:5000/"
-})
-
-export {mainAxios};
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,42 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { mainAxios } from "../../axios";
-
 const SignUp = () =>{
-    const navigate = useNavigate();
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [rePassword, setRePassword] = useState("");
-      
-    function userResgister(){
-      const result = fetch("http://localhost:5000/user/signup", {
-          method: "post",
-          body: JSON.stringify({name, email, password, rePassword}),
-          headers: {
-            "Content-Type": "application/json",
-        }
-      }).then(res => {
-        console.log("qwertyuiopasdfghjkxcvbnmsdfgbhnjm");
-        navigate("/");
-      }).catch(err => {
-        console.log(err)
-      })
-
-
-    }
-
     return(
         <div id="sign-in-container">
       <main className="form-signin w-100 m-auto">
         <form className="form-style">
           <h1 className="h3 mb-3 fw-normal">Sign Up for a New Account</h1>
             <div className="form-floating">
-                <input type="text" 
-                  className="form-control" 
-                  id="floatingName"
-                  onChange={e => setName(e.target.value)}
-                  placeholder="Harry Styles"/>
+                <input type="text" className="form-control" id="floatingName" placeholder="Harry Styles"/>
                 <label htmlFor="floatingName">Name</label>
             </div>
           <div className="form-floating">
@@ -45,7 +14,6 @@ const SignUp = () =>{
               className="form-control"
               id="floatingInput"
               placeholder="name@example.com"
-              onChange={e => setEmail(e.target.value)}
             />
             <label htmlFor="floatingInput">Email address</label>
           </div>
@@ -55,7 +23,6 @@ const SignUp = () =>{
               className="form-control"
               id="floatingPassword"
               placeholder="Password"
-              onChange={e => setPassword(e.target.value)}
             />
             <label htmlFor="floatingPassword">Password</label>
           </div>
@@ -65,7 +32,6 @@ const SignUp = () =>{
               className="form-control"
               id="floatingRePassword"
               placeholder="Re-enter Password"
-              onChange={e => setRePassword(e.target.value)}
             />
             <label htmlFor="floatingRePassword">Re-enter Password</label>
           </div>
@@ -74,12 +40,10 @@ const SignUp = () =>{
             <label>
               <input type="checkbox" value="remember-me" /> Remember me
             </label>
-          </div >
-          <div >
-          <button className="w-100 btn btn-lg btn-primary" type="submit" onClick={()=>userResgister()}>
+          </div>
+          <button className="w-100 btn btn-lg btn-primary" type="submit">
             Sign Up
           </button>
-          </div>
         </form>
       </main>
       <aside>
