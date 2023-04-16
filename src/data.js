f190a94ddc0e13b48402621a3984d42d758f6035
Sarvesh Dandapati <dsarvesh911@gmail.com>
2023-04-16 10:51:42 +0530

    add descriptions to each product in the database

--- src/data.js
+++ src/data.js
@@ -5,7 +5,8 @@ export const dataset = [
         size: ["Small", "Big"],
         type:[],
         price: [60, 110],
-        linkImg: '../../Images/PhotoFrame/'
+        linkImg: '../../Images/PhotoFrame/',
+        description: "Introducing our elegant and stylish photo frame, designed to showcase your cherished memories in the most beautiful way possible. Crafted with high-quality materials, our photo frame features a sleek and modern design that complements any decor. The frame is made of durable and lightweight recycled material, making it easy to hang on your wall or place on your desk or shelf."
     },
     {
         id:2,
@@ -13,7 +14,8 @@ export const dataset = [
         size: ["Small", "Big"],
         type:[],
         price: [60, 80],
-        linkImg: '../../Images/Notepad/'
+        linkImg: '../../Images/Notepad/',
+        description: "Introducing our high-quality notepad, the perfect tool for jotting down your thoughts, ideas, and to-do lists. The cover of our notepad is made of sturdy and stylish material that protects your notes from damage and ensures your notepad looks great even after frequent use. Inside, you'll find a generous number of pages, providing plenty of space for all your notes. Our notepad is available in various sizes, so you can choose the perfect one to fit your needs. Whether you prefer a small pocket-sized notepad or a larger one for your desk, we have the right size for you. Each page of our notepad features high-quality paper that is easy to write on and resists ink bleed-through, ensuring your notes stay legible and organized. "
     },
     {
         id:3,
@@ -21,7 +23,8 @@ export const dataset = [
         size: ["Small", "Big"],
         type:[],
         price: [25, 40],
-        linkImg: '../../Images/CarryBag/'
+        linkImg: '../../Images/CarryBag/',
+        description: "Our Carry Bag features a spacious main compartment that offers ample storage for your belongings. Our Carry Bag is available in various sizes and colours, so you can choose the perfect one to fit your needs and style. Whether you're commuting to work, heading to class, or running errands, our Carry Bag is designed to make your life easier. With its sleek and functional design and exceptional quality, our Carry Bag makes a perfect gift for anyone who needs to carry their essentials in style."
     },
     {
         id:4,
@@ -29,7 +32,8 @@ export const dataset = [
         size:[],
         type: ["Normal", "Round", "With Frame"],
         price: [60, 70, 80],
-        linkImg: '../../Images/PenStand/'
+        linkImg: '../../Images/PenStand/',
+        description: "Introducing our unique and eco-friendly pen stand, handcrafted from high-quality handmade paper. Our pen stand is made of handmade paper, which is produced using traditional techniques that have been passed down for generations. The paper is made from sustainable and renewable materials, making it an eco-friendly choice for those who care about the environment. The pen stand features a sturdy and durable construction, ensuring that your pens stay in place and your stand remains stable. The stand's compact size allows it to fit on any desk or table without taking up too much space."
     },
     {
         id:5,
@@ -37,7 +41,8 @@ export const dataset = [
         size:[],
         type:[],
         price: [60],
-        linkImg: '../../Images/JBox/'  
+        linkImg: '../../Images/JBox/',
+        description: "Introducing our unique and eco-friendly pen stand, handcrafted from high-quality handmade paper. Our pen stand is made of handmade paper, which is produced using traditional techniques that have been passed down for generations. The paper is made from sustainable and renewable materials, making it an eco-friendly choice for those who care about the environment. The pen stand features a sturdy and durable construction, ensuring that your pens stay in place and your stand remains stable. The stand's compact size allows it to fit on any desk or table without taking up too much space."  
     },
     {
         id:6,
@@ -45,7 +50,8 @@ export const dataset = [
         size:[],
         type:[],
         price: [100],
-        linkImg: '../../Images/BoxFrame/'
+        linkImg: '../../Images/BoxFrame/',
+        description: "Introducing our unique and eco-friendly pen stand, handcrafted from high-quality handmade paper. Our pen stand is made of handmade paper, which is produced using traditional techniques that have been passed down for generations. The paper is made from sustainable and renewable materials, making it an eco-friendly choice for those who care about the environment. The pen stand features a sturdy and durable construction, ensuring that your pens stay in place and your stand remains stable. The stand's compact size allows it to fit on any desk or table without taking up too much space."
     }
     // {
     //     id:7,
