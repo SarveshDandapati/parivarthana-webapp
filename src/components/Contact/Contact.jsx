2f81249e29dc99187f0fed68d095cc59cd34b55a
Sarvesh Dandapati <dsarvesh911@gmail.com>
2023-04-18 11:11:32 +0530

    design contact page

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import "../Contact/Contact.css"
+const Contact = () => {
+  return (
+    <section className="container">
+      <div className="break-small mb-2"></div>
+      <h1 className="text-head text-center">Contact Us</h1>
+      <div className="break-small mb-5"></div>
+      <h4 className="text-para text-center">Mail us at <a href="mailto:parivarthana.csa@christuniversity.in">parivarthana.csa@christuniversity.in</a></h4>
+      <div className="break-small mb-4"></div>
+        <h4 className="text-para text-center">Phone Number: 9123456789</h4>
+    </section>
+  );
+};
+
+export default Contact;
