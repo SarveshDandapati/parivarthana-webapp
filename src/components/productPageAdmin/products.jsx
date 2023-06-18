a8799fe69b3cf9a17a5360c20f5136b3c8942dc1
AbbiJay <111283167+AbbiJay@users.noreply.github.com>
2023-06-18 12:23:38 +0530

    ProductPage

--- src/components/productPageAdmin/products.jsx
+++ src/components/productPageAdmin/products.jsx
@@ -1,2 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Col } from 'reactstrap';
+import './ProductAdmin.css'
+
+const AdminProduct = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/admin/getProducts", {
+            method: "get",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }).then((res) => res.json()).
+            then((json) => {
+                console.log(json);
+                setData(json);
+            })
+    }, [])
+    return (
+<div>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12'>
+            <table className='table'>
+              <thead>
+                <tr>
+                  <th>Image</th>
+                  <th>Title</th>
+                  <th>Price</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+              {data?.map((item) => (
+                <tr key={item._id}> 
+                  <td><img src={item.images[0]} alt="prd" /></td>
+                  <td>{item.name}</td>
+                  <td>{item.cost}</td>
+                  <td><button className="btn btn-danger">Delete</button></td>
+                </tr>      
+              ))}
+              </tbody>
+            </table>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+    </div>
+    )
+}
 
 export default AdminProduct;
